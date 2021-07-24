06e508af843978019ea44cbf1dcf3045b124229b
Aleksandra Aleksandrova <AleksandraAleksandrova@users.noreply.github.com>
2021-07-24 18:49:54 +0300

    data validation not working

--- program.py
+++ program.py
@@ -1,12 +1,13 @@
 def format_text_file(file):
     FILE = open(file, "r+", encoding="utf8")
-    text = file.read()
-
+    text = FILE.read()
+    
     index = text.find("CHAPTER I.\n")
     text[index:]
-
     index = text.find("THE END \n")
     text[:index]
+    
+    print(text)
 
     FILE.close()
 
@@ -15,24 +16,20 @@ def add_to_res_file(num_of_lines, num_of_chars):
     
     file.write("Number of lines: ")
     file.write(str(num_of_lines))
-    file.write("\n")
 
     file.write("Number of characters: ")
     file.write(str(num_of_chars))
-    file.write("\n")
-    
 
 def find_num_of_lines(file):
     num_of_lines = 0
 
     FILE = open(file, "r", encoding="utf8")
     for line in FILE:
-        num_of_lines = num_of_lines + 1
+        num_of_lines +=1
     FILE.close()
 
     print("Number of lines: ", num_of_lines)
     return num_of_lines
-
 
 def find_num_of_chars(file):
     num_of_chars = 0
@@ -45,7 +42,18 @@ def find_num_of_chars(file):
     print("Number of chars: ", num_of_chars)
     return num_of_chars
 
+def get_info(file):
+    characters = 0
+    lines = 0
+    FILE = open(data, "r")
+    #във for цикъл да се броят всички линии и знаци
+    text = FILE.read()
+    for char in text:
+        if char != "\n":
+            characters +=1
+        lines +=1
 
-num_of_lines = find_num_of_lines("text.txt")
-num_of_chars = find_num_of_chars("text.txt")
-add_to_res_file(num_of_lines, num_of_chars)
+format_text_file("text.txt")
+#num_of_lines = find_num_of_lines("text.txt")
+#num_of_chars = find_num_of_chars("text.txt")
+#add_to_res_file(num_of_lines, num_of_chars)
