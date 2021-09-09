af231b653824f729f21b68a0c24357b523d65ff8
Stefan Aleksandrov <harvi1024@gmail.com>
2021-09-09 18:42:24 +0300

    Reworked the code from the beginning. Completed: 1. Creation of class object (Chapter) 2. Created logic when to start the text read, when its new chapter, when to read the chapter name and so on... 3. Create chapters objects, which are added in list (chapters)

--- with_UI/program.py
+++ with_UI/program.py
@@ -5,22 +5,47 @@
 - след зареждане на файла да има бутон процес или нещо такова 
 - после да има втори прозорец който да представлява tree view с root-ове главите и да излиза текста при плюса
 """
+
+
 class Chapter:
-    number = 0
-    name = ""
-    text = ""
+    def __init__(self, chapter):
+        self.chapter = chapter
+        self.name = None
+        self.text = []
+
+    def append_chapter_text(self, text):
+        self.text.append(text)
+
+    def set_name(self, name):
+        self.name = name
 
 
-def get_info(file):
-    FILE = open(file, "r", encoding="utf8")
-    text = FILE.read()
-    text = text[text.find("CHAPTER I.\nDown the Rabbit-Hole"):]
-    text = text[:text.find("*** END OF THE PROJECT GUTENBERG EBOOK ALICE’S ADVENTURES IN WONDERLAND ***")]
-    FILE.close()
+def read_file(chapters):
+    start_read = False
+    read_chapter_name = False
 
-    for line in text:
-        if line.find("CHAPTER") !=-1:
-            
+    with open("text.txt", 'r', encoding="utf8") as inp:
+        for index, line in enumerate(inp):
+            if line.startswith('CHAPTER'):
+                if start_read:
+                    chapters.append(chapter_object)
+                start_read = True
+                read_chapter_name = True
+                chapter_object = Chapter(line)
+            elif line.startswith('THE END'):
+                start_read = False
+            elif read_chapter_name:
+                chapter_object.set_name(line)
+                read_chapter_name = False
+            elif start_read:
+                chapter_object.append_chapter_text(line)
 
 
-get_info("text.txt")
+if __name__ == '__main__':
+    chapters = []
+    read_file(chapters)
+
+    for elem in chapters:
+        print(elem.chapter)
+        print(elem.name)
+        print(elem.text)
